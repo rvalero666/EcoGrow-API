a30b6e533315609182fc2977ae802c1e52933068
rvm666 <rvm666@educa.madrid.org>
2026-03-13 20:51:36 +0100

    feat: proyecto springboot creado

---
+++ EcoGrowApi/src/main/java/org/example/ecogrowapi/EcoGrowApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecogrowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoGrowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoGrowApiApplication.class, args);
+	}
+
+}
